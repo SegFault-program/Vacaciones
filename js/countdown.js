79f3386ad8f7a90712d4cfd0b0f74f41b5b5ee31
SegFault-program <danny0426ster@gmail.com>
2026-05-22 15:45:56 -0500

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
-    year: 2024, // required
-    month: 12, // required
-    day: 2, // required
+    year: 2026, // required
+    month: 10, // required
+    day: 12, // required
     hours: 19, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
